ab598054e5187e0f0844f18e1ed3e27feefbb3a6
XiFanYin <807142217@qq.com>
2018-08-02 18:20:12 +0800

    提交分享库

---
+++ share/src/main/java/testcomponent/local/zz/share/wxapi/WXEntryActivity.java
@@ -0,0 +1,10 @@
+package testcomponent.local.zz.share.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * Created by Administrator on 2018/8/2.
+ */
+
+public class WXEntryActivity extends WXCallbackActivity {
+}
