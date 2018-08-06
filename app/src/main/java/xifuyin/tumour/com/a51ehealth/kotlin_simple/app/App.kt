a7201d788d9c5d24afd2a9eb9738372b934865f1
XiFanYin <807142217@qq.com>
2018-08-06 10:48:15 +0800

    统计

--- app/src/main/java/xifuyin/tumour/com/a51ehealth/kotlin_simple/app/App.kt
+++ app/src/main/java/xifuyin/tumour/com/a51ehealth/kotlin_simple/app/App.kt
@@ -4,9 +4,7 @@ import android.app.Application
 import com.squareup.leakcanary.LeakCanary
 import com.umeng.analytics.MobclickAgent
 import com.umeng.commonsdk.UMConfigure
-import com.umeng.commonsdk.stateless.UMSLEnvelopeBuild.mContext
 import com.umeng.socialize.PlatformConfig
-import com.umeng.socialize.UMShareAPI
 
 /**
  * Created by Administrator on 2018/5/21.
@@ -34,12 +32,12 @@ class App : Application() {
     //初始化友盟
     private fun initUmeng() {
 
-        UMConfigure.init(this, "5b61764ef43e483fda000131", "umeng", UMConfigure.DEVICE_TYPE_PHONE, "")
+        UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, null)
         /*这些也需要去替换 **/
         PlatformConfig.setWeixin("wx6c609010c5101349", "c896637e1b9e97756f3ca156b64f7aa1")
         PlatformConfig.setQQZone("1107474610", "frEHNKoKEFjtZkZK")
-        UMConfigure.setLogEnabled(true)
-
+        //这里配置log输出信息
+        UMConfigure.setLogEnabled(false)
         //统计用的，不知道管什么用
         MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL)
     }
